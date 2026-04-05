090bed690a022f232d679076d7abd73b7e0afa90
Poorna Chandra D <poornacd24@gmail.com>
2026-04-05 00:23:35 -0400

    docs: Add commit documentation and project metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+"""
+LLM Gateway Setup Configuration
+Last Updated: April 5, 2026
+Latest Commit: b1e6990 - chore: Remove .gitattributes
+Version: 1.0.0 - Complete project cleanup and optimization
+Repository: https://github.com/Poorna-Chandra-D/LLM-Gateway
+Author: Poorna Chandra D <poornacd24@gmail.com>
+"""
 from setuptools import setup, find_packages
 
 setup(
