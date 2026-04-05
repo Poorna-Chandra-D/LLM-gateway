d81db1e11903d722790e1632781ba05eeba48298
Poorna Chandra D <poornacd24@gmail.com>
2026-04-05 00:31:34 -0400

    chore: Update configuration files and documentation

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 """
 LLM Gateway Setup Configuration
-Last Updated: April 5, 2026
-Latest Commit: b1e6990 - chore: Remove .gitattributes
-Version: 1.0.0 - Complete project cleanup and optimization
 Repository: https://github.com/Poorna-Chandra-D/LLM-Gateway
 Author: Poorna Chandra D <poornacd24@gmail.com>
 """
